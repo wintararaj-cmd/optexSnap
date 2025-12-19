4ef16cf22b4f00628e538eca31250cd0af0835c7
Trishita <trishita@ruchi.com>
2025-12-19 22:53:25 +0530

    Add migration to fix expenses table schema

---
+++ database/migrations/fix_expenses_table.sql
@@ -0,0 +1,22 @@
+
+-- Add missing columns to expenses table if they don't exist
+DO $$
+BEGIN
+    BEGIN
+        ALTER TABLE expenses ADD COLUMN date DATE NOT NULL DEFAULT CURRENT_DATE;
+    EXCEPTION
+        WHEN duplicate_column THEN NULL;
+    END;
+
+    BEGIN
+        ALTER TABLE expenses ADD COLUMN receipt_image_id INTEGER REFERENCES images(id) ON DELETE SET NULL;
+    EXCEPTION
+        WHEN duplicate_column THEN NULL;
+    END;
+
+    BEGIN
+        ALTER TABLE expenses ADD COLUMN notes TEXT;
+    EXCEPTION
+        WHEN duplicate_column THEN NULL;
+    END;
+END $$;
